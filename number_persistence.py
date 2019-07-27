526bddf48b31853a5e4af1be08cb650403618cb2
danielwhomer <daniel.w.homer@gmail.com>
2019-07-27 15:00:38 -0700

    updated number_persistence with comments

--- number_persistence.py
+++ number_persistence.py
@@ -1,8 +1,18 @@
 import sys
 
+#https://en.wikipedia.org/wiki/Persistence_of_a_number
+
+#Calculates the number of steps it takes to reduce a number down to a single digit by multiplying its digits
+#For example when n = 39
+#n = 3*9
+#n = 2*7
+#n = 1*4
+#n = 4
+
+#the current known most persistent number is 277777788888899 (11 steps)
+
 def persistence(number, steps):
 	if len(str(number)) == 1:
-		print(number)
 		return "DONE"
 	digits = [int(i) for i in str(number)]
 
