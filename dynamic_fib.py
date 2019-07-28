75d49dc8415693f70bac34e27b102e7b1635e9cd
danielwhomer <daniel.w.homer@gmail.com>
2019-07-28 08:59:57 -0700

    added command line input

--- dynamic_fib.py
+++ dynamic_fib.py
@@ -1,6 +1,12 @@
 #This implementation of the fibonacci sequence uses 'memoization'
 #known values are recorded in a dictionary and checked before new values are calcuated
 #i.e. if we know fib(10), why calculate it again each time n > 10
+#memoized calls cost constant time.
+#the number of non-memoized calls is equal to n:
+#fib(1), fib(2),...,fib(n)
+#the non recursive work per call is constant
+#the running time is therefore linear
+import sys
 
 memo = {}
 
@@ -14,4 +20,4 @@ def fib(n):
 	memo[n] = f
 	return f
 
-print(fib(6))
+print(fib(int(sys.argv[1])))
